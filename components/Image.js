e904e8987b4d72a9f78b383ecff99893cbb4a6cf
id <jeremy.rey@inthememory.com>
2022-11-08 12:57:30 +0100

    fix image et lang

--- components/Image.js
+++ components/Image.js
@@ -2,8 +2,11 @@ import { Image as I } from '@chakra-ui/react'
 import { storyblokEditable } from "@storyblok/react";
 
 const Image = ({ blok }) => {
+  let _props = '{}'
+  if (blok.props != '')
+    _props = blok.props
   return (
-    <I src={blok.src.filename} alt={blok.alt} />
+    <I src={blok.src.filename} alt={blok.alt} htmlWidth={blok.width} htmlHeight={blok.height} {...JSON.parse(_props)}/>
   );
 };
 
--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 // next.config.js
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a.storyblok.com',
+      },
+    ],
+  },
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
