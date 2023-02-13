a978c2a509ff398d6bbb771df2442053d9b70f04
id <jeremy.rey@inthememory.com>
2023-02-13 22:31:49 +0100

    poc rich text

--- components/Text.js
+++ components/Text.js
@@ -1,4 +1,4 @@
-import { Text as T } from '@chakra-ui/react'
+import { Box as B } from '@chakra-ui/react'
 import { storyblokEditable } from '@storyblok/react'
 import propsToJson from '../hooks/propsToJson'
 import { render } from 'storyblok-rich-text-react-renderer'
@@ -6,7 +6,11 @@ import { render } from 'storyblok-rich-text-react-renderer'
 const Text = ({ blok }) => {
   let jsonParams = propsToJson(blok.props)
 
-  return <div>{render(blok.content)}</div>
+  return (
+    <B {...storyblokEditable(blok)} key={blok._uid} {...jsonParams}>
+      {render(blok.content)}
+    </B>
+  )
 }
 
 export default Text
