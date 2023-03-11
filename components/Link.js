054deaaaad300248198d9237589bbf6af1fbcf18
Jeremy Rey <jeremyrey@aplusc.fr>
2023-03-11 15:20:11 +0100

    chore(link): add props

--- components/Link.js
+++ components/Link.js
@@ -3,8 +3,14 @@ import { Link as L } from '@chakra-ui/react'
 import propsToJson from '../hooks/propsToJson'
 
 const Link = ({ blok }) => {
+  let jsonParams = propsToJson(blok.props)
   return (
-    <L href={blok.link.cached_url} {...storyblokEditable(blok)} key={blok._uid}>
+    <L
+      href={blok.link.cached_url}
+      {...storyblokEditable(blok)}
+      key={blok._uid}
+      {...jsonParams}
+    >
       {blok.content.map((blok) => (
         <StoryblokComponent blok={blok} key={blok._uid} />
       ))}
--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -1,4 +1,26 @@
+import sendgrid from '@sendgrid/mail'
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
+
 async function sendEmail(req, res) {
+  try {
+    // console.log("REQ.BODY", req.body);
+    await sendgrid.send({
+      to: 'mannuarora7000@gmail.com', // Your email where you'll receive emails
+      from: 'manuarorawork@gmail.com', // your website email address here
+      subject: `${req.body.subject}`,
+      html: `<div>${req.body.message}</div>`,
+    })
+  } catch (error) {
+    // console.log(error);
+    return res.status(error.statusCode || 500).json({ error: error.message })
+  }
+
+  return res.status(200).json({ error: '' })
+}
+
+export default sendEmail
+/*async function sendEmail(req, res) {
   let nodemailer = require('nodemailer')
   const transporter = nodemailer.createTransport({
     port: 465,
@@ -23,4 +45,4 @@ async function sendEmail(req, res) {
   res.status(200)
 }
 
-export default sendEmail
+export default sendEmail*/
