1de2185792eceb5901c7a6284de440c0a29d5768
Jeremy Rey <jeremyrey@aplusc.fr>
2024-03-30 11:21:16 +0100

    feat(Carousel): add arrow opacity

--- components/Carousel.js
+++ components/Carousel.js
@@ -3,16 +3,33 @@ import { Box, Flex } from '@chakra-ui/react'
 import { useRef, useState } from 'react'
 import propsToJson from '../hooks/propsToJson'
 
+const ARROW_DISABLED_OPACITY = 0.3
+
 const Carousel = ({ blok }) => {
   const jsonParams = propsToJson(blok.props)
   const containerJsonParams = propsToJson(blok.container_props)
   const elementRef = useRef(null)
+  const [arrowLeftDisabled, setArrowLeftDisabled] = useState(true)
+  const [arrowRightDisabled, setArrowRightDisabled] = useState(false)
 
   const handleHorizantalScroll = (speed, distance, step) => {
     let scrollAmount = 0
     const slideTimer = setInterval(() => {
       elementRef.current.scrollLeft += step
       scrollAmount += Math.abs(step)
+      if (elementRef.current.scrollLeft <= 0) {
+        setArrowLeftDisabled(true)
+      } else {
+        setArrowLeftDisabled(false)
+      }
+      if (
+        elementRef.current.scrollLeft >=
+        elementRef.current.scrollWidth - elementRef.current.clientWidth
+      ) {
+        setArrowRightDisabled(true)
+      } else {
+        setArrowRightDisabled(false)
+      }
       if (scrollAmount >= distance) {
         clearInterval(slideTimer)
       }
@@ -22,6 +39,7 @@ const Carousel = ({ blok }) => {
   return (
     <Box {...jsonParams}>
       <Box
+        opacity={arrowLeftDisabled ? ARROW_DISABLED_OPACITY : 1}
         onClick={() =>
           handleHorizantalScroll(
             25,
@@ -36,6 +54,7 @@ const Carousel = ({ blok }) => {
         />
       </Box>
       <Box
+        opacity={arrowRightDisabled ? ARROW_DISABLED_OPACITY : 1}
         onClick={() =>
           handleHorizantalScroll(
             25,
