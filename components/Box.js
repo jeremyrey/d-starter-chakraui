11b1cb50354a93f74f9723690511a806fdfdcf2a
Jeremy Rey <jeremy.rey@delupay.com>
2025-06-20 08:22:37 +0200

    feat: add motion for animation

--- components/Box.js
+++ components/Box.js
@@ -1,16 +1,18 @@
 import { StoryblokComponent, storyblokEditable } from '@storyblok/react'
-import { Box as B } from '@chakra-ui/react'
+import { chakra } from '@chakra-ui/react'
 import propsToJson from '../hooks/propsToJson'
+import { motion } from 'motion/react'
 
 const Box = ({ blok }) => {
   const jsonParams = propsToJson(blok.props)
+  const MotionBox = chakra(motion.div)
 
   return (
-    <B {...storyblokEditable(blok)} key={blok._uid} {...jsonParams}>
+    <MotionBox {...storyblokEditable(blok)} key={blok._uid} {...jsonParams}>
       {blok.content.map((blok) => (
         <StoryblokComponent blok={blok} key={blok._uid} />
       ))}
-    </B>
+    </MotionBox>
   )
 }
 
